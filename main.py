a921bf64a21b860fae0f9116813bf98db919320e
Philippe Carpentier-Savard <philippe.carpentier-savard.1@ulaval.ca>
2019-12-12 22:23:44 -0500

    Boucle principale

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 """Quoridor - module main"""
 import argparse
+import re
+import time
+import turtle
+
+import api
+from quoridor import Quoridor
+from quoridorx import QuoridorX
 
 
 def analyser_commande():
@@ -18,6 +25,93 @@ def analyser_commande():
     parser.add_argument("-ax", dest="gui_auto", action="store_true",
                         help="Jouer en mode automatique contre le serveur avec affichage graphique")
 
-    parser.add_argument("idul", help="IDUL du joueur")
+    parser.add_argument("idul", help="IDUL du joueur")  #, nargs='?', default="phcas16")
 
     return parser.parse_args()
+
+
+def main():
+    """Boucle principale."""
+    args = analyser_commande()
+
+    if args.lister:
+        for partie in api.lister_parties(args.idul):
+            print(partie["id"])
+        return
+
+    id_partie, partie = api.débuter_partie(args.idul)
+    gagnant = False
+    q = None
+
+    mode_auto = args.console_auto or args.gui_auto or True
+    mode_graphique = args.gui_manuel or args.gui_auto or True
+
+    while not gagnant:
+        if mode_graphique:
+            q = QuoridorX(partie["joueurs"], partie["murs"])
+        else:
+            q = Quoridor(partie["joueurs"], partie["murs"])
+
+        gagnant = q.partie_terminée()
+        if gagnant:
+            break
+
+        if mode_auto:
+            time.sleep(0.25)
+            q.jouer_coup(1)
+
+        if mode_graphique:
+            q.afficher()
+        else:
+            print("", q, sep="\n")
+
+        if mode_auto:
+            partie = api.jouer_coup(id_partie, q.type_coup.upper(), q.pos_coup)
+        else:
+            capture = None
+            titre = "C'est votre tour!"
+            question = "Entrez votre prochain coup sous la forme (D|MH|MV) x y :"
+
+            while not capture:
+                if mode_graphique:
+                    entree = turtle.textinput(titre, question)
+                    if entree is None:  # bouton Cancel ou X
+                        turtle.mainloop()  # pause sur damier
+                        raise RuntimeError("Fenêtre fermée par le joueur")
+                else:
+                    print(question, end=" ")
+                    entree = input()
+
+                capture = re.search(r'^(D|MH|MV)\s+(\d)\s+(\d)$', entree.strip(), re.IGNORECASE)
+
+                if capture:
+                    entrees = capture.groups()
+
+                    try:
+                        partie = api.jouer_coup(id_partie, entrees[0].upper(),
+                                                (int(entrees[1]), int(entrees[2])))
+                    # except StopIteration as gagnant:
+                    #    gagnant = str(gagnant)
+                    except RuntimeError as message:
+                        titre = str(message)
+                        capture = None
+                        if not mode_graphique:
+                            print(titre)
+                else:
+                    titre = "Erreur de syntaxe, veuillez réessayer."
+                    if not mode_graphique:
+                        print(titre)
+
+    if mode_graphique:
+        turtle.bgcolor("forestgreen" if gagnant == partie["joueurs"][0]["nom"] else "firebrick")
+    else:
+        print("", q, sep="\n")
+
+    print(f'\n{gagnant} a gagné la partie!\n')
+
+    if mode_graphique:
+        turtle.mainloop()  # pause sur damier
+
+
+if __name__ == "__main__":
+    main()
--- quoridor.py
+++ quoridor.py
@@ -1,4 +1,4 @@
-"""Quoridor - étape 2 - module quoridor"""
+"""Quoridor - module quoridor"""
 from copy import deepcopy
 import networkx as nx
 
@@ -123,7 +123,7 @@ class Quoridor:
                    for j, mur_v2 in enumerate(murs_v) if i != j):
                 raise QuoridorError("Deux des murs verticaux se chevauchent")
 
-            if any(mur_h == (mur_v[0] - 1, mur_v[1] + 1) for mur_h in murs_h):
+            if any(tuple(mur_h) == (mur_v[0] - 1, mur_v[1] + 1) for mur_h in murs_h):
                 raise QuoridorError("Un des murs horizontaux et un des murs verticaux se "
                                     "chevauchent")
 
@@ -162,6 +162,8 @@ class Quoridor:
 
         nb_murs = 0
         self.etat = {"joueurs": [], "murs": None}
+        self.type_coup = ""
+        self.pos_coup = None
 
         for i, joueur in enumerate(joueurs):
             if isinstance(joueur, dict):
@@ -350,7 +352,7 @@ class Quoridor:
         chemin_adversaire = nx.shortest_path(graphe, pos_adversaire, f'B{adversaire}')
         deplacer_joueur = False
 
-        if len(chemin_joueur) < len(chemin_adversaire) or \
+        if len(chemin_joueur) <= len(chemin_adversaire) or \
                 len(graphe.successors(pos_adversaire)) < 2:
             deplacer_joueur = True
         else:
@@ -362,10 +364,14 @@ class Quoridor:
                 mur_v = [prochaine_pos_adversaire[0] - min(diff_x, 0), prochaine_pos_adversaire[1]]
                 try:
                     self.placer_mur(joueur, tuple(mur_v), "vertical")
+                    self.type_coup = "MV"
+                    self.pos_coup = tuple(mur_v)
                 except QuoridorError:
                     mur_v[1] -= 1  # tentative plaçage mur vertical plus bas
                     try:
                         self.placer_mur(joueur, tuple(mur_v), "vertical")
+                        self.type_coup = "MV"
+                        self.pos_coup = tuple(mur_v)
                     except QuoridorError:
                         deplacer_joueur = True
 
@@ -373,15 +379,21 @@ class Quoridor:
                 mur_h = [prochaine_pos_adversaire[0], prochaine_pos_adversaire[1] - min(diff_y, 0)]
                 try:
                     self.placer_mur(joueur, tuple(mur_h), "horizontal")
+                    self.type_coup = "MH"
+                    self.pos_coup = tuple(mur_h)
                 except QuoridorError:
                     mur_h[0] -= 1  # tentative plaçage mur horizontal plus à gauche
                     try:
                         self.placer_mur(joueur, tuple(mur_h), "horizontal")
+                        self.type_coup = "MH"
+                        self.pos_coup = tuple(mur_h)
                     except QuoridorError:
                         deplacer_joueur = True
 
         if deplacer_joueur:
             self.déplacer_jeton(joueur, chemin_joueur[1])
+            self.type_coup = "D"
+            self.pos_coup = chemin_joueur[1]
 
     def partie_terminée(self):
         """
